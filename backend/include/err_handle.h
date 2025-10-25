3377a9a115ceb7539c815f58981dc76b6ce22a09
JimGav <dimitrisgav04@gmail.com>
2025-10-25 11:25:06 +0300

    Add err_handle.h

---
+++ backend/include/err_handle.h
@@ -0,0 +1,25 @@
+#ifndef ERR_HANDLE_H
+#define ERR_HANDLE_H
+
+#define PSTAT(stat, label){                 \
+    if (stat == STAT_NULLPTR){              \
+        printf("%s: STAT_NULLPTR\n", label);  \
+    }                                       \
+    if (stat == STAT_ERR){                  \
+        printf("%s: STAT_ERR\n", label);      \
+    }                                       \
+    if (stat == STAT_NOTFOUND){                  \
+        printf("%s: STAT_NOTFOUND\n", label);      \
+    }                                       \
+    exit(stat);                             \
+}                                           \
+
+
+typedef enum {
+    STAT_SUCCESS,
+    STAT_NULLPTR,
+    STAT_ERR,
+    STAT_NOTFOUND
+} status_t;
+
+#endif
