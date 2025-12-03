7b64b2a3b2691ce90a6cd7ad9f68bae31bf7cb6a
JimGav <dimitrisgav04@gmail.com>
2025-12-04 01:18:11 +0200

    Add set_one and set_zero to bb

--- backend/include/bitboard.h
+++ backend/include/bitboard.h
@@ -2,7 +2,13 @@
 #define BITBOARD_H
 
 #include <stdint.h>
+#include <stdbool.h>
+#include "sqr.h"
+#include "err_handle.h"
 
 #define BB_t uint64_t
+
+status_t set_one(BB_t *bb, sqr_t sqr);
+status_t set_zero(BB_t *bb, sqr_t sqr);
 
 #endif
--- backend/src/bitboard.c
+++ backend/src/bitboard.c
@@ -1,1 +1,11 @@
 #include "bitboard.h"
+
+status_t set_one(BB_t *bb, sqr_t sqr){
+    BB_t x = 1;
+    *bb = *bb | (x << sqr);
+}
+
+status_t set_zero(BB_t *bb, sqr_t sqr){
+    BB_t x = ~1;
+    *bb = *bb & (x << sqr);
+}
