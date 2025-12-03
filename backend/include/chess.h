5f7fafc707aa04609c060b526e5c87a8cc1f2a39
JimGav <dimitrisgav04@gmail.com>
2025-12-04 00:35:11 +0200

    typedef enums

--- backend/include/chess.h
+++ backend/include/chess.h
@@ -7,12 +7,12 @@
 #include "list.h"
 
 
-enum enumColor{
+typedef enum {
     WHITE, BLACK
-};
+} color_t;
 
 
-enum enumSquare {
+typedef enum {
   a1, b1, c1, d1, e1, f1, g1, h1,
   a2, b2, c2, d2, e2, f2, g2, h2,
   a3, b3, c3, d3, e3, f3, g3, h3,
@@ -21,10 +21,10 @@ enum enumSquare {
   a6, b6, c6, d6, e6, f6, g6, h6,
   a7, b7, c7, d7, e7, f7, g7, h7,
   a8, b8, c8, d8, e8, f8, g8, h8
-};
+} sqr_t;
 
 
-enum enumPiece {
+typedef enum {
     WHITE_PAWN,
     WHITE_ROOK,
     WHITE_KNIGHT,
@@ -37,14 +37,14 @@ enum enumPiece {
     BLACK_BISHOP,
     BLACK_QUEEN,
     BLACK_KING
-};
+} piece_t;
 
 
 typedef struct {
     BB_t piece_bbs[12];
-    enum enumColor turn;
+    color_t turn;
     bool castling_rights[2];
-    enum enumSquare ep_target;
+    sqr_t ep_target;
 } ChessState;
 
 
