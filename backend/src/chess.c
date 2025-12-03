827bbebce57be75df6d3c19fa4364876e9a9c725
JimGav <dimitrisgav04@gmail.com>
2025-12-04 01:20:39 +0200

    Add gen_start_state

--- backend/src/chess.c
+++ backend/src/chess.c
@@ -1,1 +1,47 @@
+#include <string.h>
 #include "chess.h"
+#include "sqr.h"
+#include "bitboard.h"
+#include "err.h"
+
+status_t gen_start_state(ChessState *start_state){
+
+    if (start_state == NULL)
+        return STAT_NULLPTR;
+
+    memset(start_state, 0, sizeof(ChessState));
+    
+    BB_t *piece_bbs = start_state->piece_bbs;
+
+    set_one(&piece_bbs[WHITE_ROOK], a1);
+    set_one(&piece_bbs[WHITE_ROOK], h1);
+    set_one(&piece_bbs[WHITE_KNIGHT], b1);
+    set_one(&piece_bbs[WHITE_KNIGHT], g1);
+    set_one(&piece_bbs[WHITE_BISHOP], c1);
+    set_one(&piece_bbs[WHITE_BISHOP], f1);
+    set_one(&piece_bbs[WHITE_QUEEN], d1);
+    set_one(&piece_bbs[WHITE_KING], e1);
+
+    set_one(&piece_bbs[BLACK_ROOK], a8);
+    set_one(&piece_bbs[BLACK_ROOK], h8);
+    set_one(&piece_bbs[BLACK_KNIGHT], b8);
+    set_one(&piece_bbs[BLACK_KNIGHT], g8);
+    set_one(&piece_bbs[BLACK_BISHOP], c8);
+    set_one(&piece_bbs[BLACK_BISHOP], f8);
+    set_one(&piece_bbs[BLACK_QUEEN], d8);
+    set_one(&piece_bbs[BLACK_KING], e8);
+
+    for (int i = 0; i < 8; i++){
+        set_one(&piece_bbs[WHITE_PAWN], a1+i);
+        set_one(&piece_bbs[BLACK_PAWN], a8+i);
+    }
+
+    start_state->castling_rights[WHITE] = 0;
+    start_state->castling_rights[BLACK] = 0;
+    
+    start_state->ep_target = 0;
+    
+    start_state->turn = WHITE;
+
+    return STAT_SUCCESS;
+}
