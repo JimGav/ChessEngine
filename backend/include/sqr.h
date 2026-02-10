7b3bd22b19699da205e23d39eb4802ed61cb677e
JimGav <dimitrisgav04@gmail.com>
2026-02-10 15:07:30 +0200

    Add sqr macros for valid sqr checking and compass moving

--- backend/include/sqr.h
+++ backend/include/sqr.h
@@ -1,6 +1,60 @@
 #ifndef SQR_H
 #define SQR_H
 
+#define VALID_SQR(sqr) (sqr >= a1 && sqr <= h8)
+
+#define N(sqr, i) {\
+  if (RANK(sqr) > 8-i)\
+    return -1;\
+  else\
+    return (sqr+8*i);\
+}\
+
+#define W(sqr, j) {\
+  if (FILE(sqr) < 1+j)\
+    return -1;\
+  else\
+    return (sqr-j);\
+}\
+
+#define E(sqr, j) {\
+  if (FILE(sqr) > 8-j)\
+    return -1;\
+  else\
+    return (sqr+j);\
+}\
+
+#define S(sqr, i) {\
+  if (RANK(sqr) < 1+i)\
+    return -1;\
+  else\
+    return (sqr-8*i);\
+}\
+
+#define NW(sqr, i) {\
+  if (N(sqr,i) == -1)\
+    return -1\
+  return W(N(sqr,i),i);\
+}\
+
+#define NE(sqr, i) {\
+  if (N(sqr,i) == -1)\
+    return -1\
+  return E(N(sqr,i),i);\
+}\
+
+#define SW(sqr, i) {\
+  if (S(sqr,i) == -1)\
+    return -1\
+  return W(S(sqr,i),i);\
+}\
+
+#define SE(sqr, i) {\
+  if (S(sqr,i) == -1)\
+    return -1\
+  return E(S(sqr,i),i);\
+}\
+
 typedef enum {
   a1, b1, c1, d1, e1, f1, g1, h1,
   a2, b2, c2, d2, e2, f2, g2, h2,
