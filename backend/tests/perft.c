25bba285fb6acd38f14dfeec9d67c6981a9e81ad
JimGav <dimitrisgav04@gmail.com>
2026-02-20 13:38:07 +0200

    Fix perft

--- backend/tests/perft.c
+++ backend/tests/perft.c
@@ -5,34 +5,32 @@
 
 typedef unsigned long long u64;
 
-u64 perft(int depth)
+
+
+u64 perft(ChessState state, int depth)
 {
-    List *list = list_create(compare_moves, NULL);
-    int n_moves, i;
-    u64 nodes = 0;
+    if (depth == 0)
+        return 1ULL;
 
-    if (depth == 0) 
-    return 1ULL;
-
-    ChessState state;
-    gen_start_state(&state);
-    gen_attck_bbs();
-    n_moves = gen_legal_moves(&state, list);
-
-    ListNode *node = list->head;
-    while (node){
-        Move *move = node->dt_ptr;
-        ChessState dummy_state = state;
-        play_move(move, &dummy_state);
-        nodes += perft(depth - 1);
-        node = node->next;
+    List *succ = list_create(NULL, NULL);
+    gen_successors(&state, succ);
+    
+    int n = 0;
+    for (int i = 0; i < succ->size; i++){
+        ChessState s = *((ChessState*)succ->head->dt_ptr);
+        n += perft(s, depth-1);
+        succ->head = succ->head->next;
     }
 
-    return nodes;
+    return n;
 }
 
 
 int main(int argc, char *argv[]){
-    for (int i = 0; i < atoi(argv[1]); i++)
-        printf("PERFT(%d) = %lld\n", i, perft(i));
+    for (int i = 0; i < atoi(argv[1]); i++){
+        ChessState cstate;
+        gen_start_state(&cstate);
+        gen_attck_bbs();
+        printf("PERFT(%d) =  %lld\n", i, perft(cstate, i));
+    }
 }
