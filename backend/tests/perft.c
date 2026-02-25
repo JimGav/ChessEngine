fa3cadd90ae701d01b43f4e1747b182b762e6b75
JimGav <dimitrisgav04@gmail.com>
2026-02-25 02:19:02 +0200

    Modify perft to check captures,ep,castles

--- backend/tests/perft.c
+++ backend/tests/perft.c
@@ -8,21 +8,38 @@ typedef unsigned long long u64;
 
 
 
-u64 perft(ChessState state, int depth)
+u64 perft(ChessState state, int depth, int *captures, int *ep, int *castles)
 {
-    if (depth == 0)
-        return 1ULL;
-
-    List *succ = list_create(NULL, NULL);
-    gen_successors(&state, succ);
+    List *moves = list_create(compare_moves, destroy_move);
+    gen_legal_moves(&state, moves);
     
-    int n = 0;
-    for (int i = 0; i < succ->size; i++){
-        ChessState s = *((ChessState*)succ->head->dt_ptr);
-        n += perft(s, depth-1);
-        succ->head = succ->head->next;
+    if (depth == 0){
+        list_destroy(moves);
+        return 1ULL;
     }
 
+    int n = 0;
+    ListNode *node = moves->head;
+    for (int i = 0; i < moves->size; i++){
+        Move *move = node->dt_ptr;
+
+        if (depth == 1){
+            if (move->castling != NO_CASTLING)
+                *castles = *castles+1;
+            if (get_piece_on_sqr(&state, move->target) != PIECE_T_LAST)
+                *captures = *captures+1;
+            if (move->is_ep){print_move(*move);
+                *ep = *ep+1;}
+
+        }
+
+        ChessState dstate = state;
+        make_move_on(move, &dstate);
+        n += perft(dstate, depth-1, captures, ep, castles);
+        node = node->next;
+    }
+
+    list_destroy(moves);
     return n;
 }
 
@@ -32,14 +49,29 @@ int main(int argc, char *argv[]){
     u64 perft_res[] = {
         1, 20, 400, 8902, 197281, 4865609, 119060324, 3195901860, 84998978956, 2439530234167, 69352859712417
     };
+    u64 perft_captures[] = {
+        0, 0, 0, 34, 1576, 82719, 2812008, 108329926, 3523740106, 125208536153
+    };
+    u64 perft_castles[] = {
+        0, 0, 0, 0, 0, 0, 0, 883453, 23605205, 1784356000	
+    };
+    u64 perft_ep[] = {
+        0, 0, 0, 0, 0, 258, 5248, 319617, 7187977, 319496827
+    };
 
-    for (int i = 0; i < atoi(argv[1]); i++){
+    for (int i = 0; i <= atoi(argv[1]); i++){
         ChessState cstate;
         gen_start_state(&cstate);
         gen_attck_bbs();
         
-        u64 p = perft(cstate, i);
-        printf("PERFT(%d) =  %lld\n", i, p);
+        int captures = 0;
+        int castles = 0;
+        int ep = 0;
+        u64 p = perft(cstate, i, &captures, &ep, &castles);
+        printf("PERFT(%d) =  %lld Captures:%d, EP:%d, Castles:%d\n", i, p, captures, ep, castles);
         assert(p == perft_res[i]);
+        assert(captures == perft_captures[i]);
+        assert(ep == perft_ep[i]);
+        assert(castles == perft_castles[i]);
     }
 }
